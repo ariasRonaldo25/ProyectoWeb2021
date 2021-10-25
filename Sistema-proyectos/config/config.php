8d09d6e478116654a0dcdacc4718a64fef86e702
Ronaldo Arias Parada <ronaldorafaelap@ufps.edu.co>
2021-10-25 10:15:39 -0500

    agregando archivo config

---
+++ Sistema-proyectos/config/config.php
@@ -0,0 +1,9 @@
+
+<?php
+
+define('URL', 'http://localhost:80/Sistema-proyectos/');
+define('HOST', 'localhost');
+define('DB', 'mydb');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8');
